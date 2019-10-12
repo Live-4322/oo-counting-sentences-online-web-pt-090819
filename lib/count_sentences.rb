aa905b9baff0b3e7ea6173240e3ddd11606aae23
Charlie Oster <oster.charlie@yahoo.com>
2019-10-12 20:51:47 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,26 @@
 require 'pry'
 
 class String
-
-  def sentence?
-    
+  
+   def sentence 
+    "Hi, my name is Sophie.".sentence
+  
   end
 
-  def question?
+  def question
+    "What is your name".question
 
   end
 
   def exclamation?
+    "Happy Halloween!".exclamation
 
   end
 
   def count_sentences
+    count_sentences
 
+   end
   end
-end
+ end
+ 
